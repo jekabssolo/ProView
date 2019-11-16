900d531ad83ef8f894ebce08bb69fda0ac851a2c
jekabssoloRBS <jekabs.solovjovs@rbs.lv>
2019-11-16 04:25:26 +0200

    Updating works

--- functions/update.php
+++ functions/update.php
@@ -2,15 +2,19 @@
   require_once "connect.php";
 
   function updateExisting($id, $name, $financer, $status, $number, $sam, $budget, $budgetspent, $purpose, $activities, $sdate, $fdate, $cname, $ccontact) {
+
+
     global $mysqli;
     connectDB();
 
     $query = "UPDATE projekti
-              SET Name = $name, Financer = $financer, Status = $status, Number = $number, SAM = $sam, Budget = $budget, Purpose = $purpose, Activities = $activities, StartDate = $sdate, FinishDate = $fdate, CoordinatorName = $cname, CoordinatorContacts = $ccontact, BudgetSpent = $budgetspent
-              WHERE ID = $id;";
+    SET Name = '$name', Financer = '$financer', Status = '$status', Number = '$number', SAM = '$sam', Budget = '$budget', BudgetSpent = '$budgetspent', Purpose = '$purpose', Activities = '$activities', StartDate = '$sdate', FinishDate = '$fdate', coordinatorName = '$cname', CoordinatorContacts = '$ccontact'
+    WHERE ID = $id;";
     
     mysqli_query($mysqli, $query);
 
     closeDB();
+    $respons = "Saglabāts";
+    return $respons;
   }
 ?>
